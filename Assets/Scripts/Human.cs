6bd9b556f9cc373fff2d6ea09352ccf5dcb4b4d6
Nicholas-C-Brown <brown.nicholas360@gmail.com>
2023-04-29 10:49:52 -0700

    splatter events

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -2,35 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Human : MonoBehaviour
+public class Human : AbstractKillable
 {
     [SerializeField] GameObject bloodPrefab;
-    [SerializeField] Rigidbody2D rb;
-    public float accelerationTime = 2f;
-    public float maxSpeed = 5f;
-    private Vector2 movement;
-    private float timeLeft;
-
-    void Update()
-    {
-        timeLeft -= Time.deltaTime;
-        if (timeLeft <= 0)
-        {
-            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-            timeLeft += accelerationTime;
-        }
-
-
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -7, 7), Mathf.Clamp(transform.position.y, -5, 5), 0);
-
-    }
-
-    void FixedUpdate()
-    {
-        rb.AddForce(movement * maxSpeed);
-    }
-
-    public void Perish()
+    public override void Perish()
     {
         Instantiate(bloodPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,28 +8,35 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Rigidbody2D carRigidbody2D;
     [SerializeField] private float speed = 10f;
-    [SerializeField] private float turnSpeed = 1f;
-    private float usedTurnSpeed;
+    [SerializeField] private float turnSpeedMax = 1f;
+
+    public event EventHandler<OnKillEventArgs> OnKill;
+    public class OnKillEventArgs : EventArgs
+    {
+        public int Points;
+    }
+    
+    private float turnSpeed;
 
     private void FixedUpdate()
     {
 
-        usedTurnSpeed = Mathf.Lerp(usedTurnSpeed, 0f, Time.deltaTime * 5);
+        turnSpeed = Mathf.Lerp(turnSpeed, 0f, Time.deltaTime * 5);
 
         if (Input.GetKey(KeyCode.W))
         {
             carRigidbody2D.AddForce(transform.up * speed);
-            usedTurnSpeed = turnSpeed;
+            turnSpeed = turnSpeedMax;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            carRigidbody2D.AddTorque(usedTurnSpeed);
+            carRigidbody2D.AddTorque(turnSpeed);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            carRigidbody2D.AddTorque(-usedTurnSpeed);
+            carRigidbody2D.AddTorque(-turnSpeed);
         }
 
 
@@ -37,11 +45,17 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
        
-        Human human = collision.gameObject.GetComponent<Human>();
-        if (human != null)
+        AbstractKillable killable = collision.gameObject.GetComponent<AbstractKillable>();
+        if (killable != null)
         {
-            Debug.Log(human);
-            human.Perish();
+            //Invoke event
+            OnKill?.Invoke(this, new OnKillEventArgs { Points = killable.GetPoints() });
+
+            //Apply stopping force
+            carRigidbody2D.AddForce(-carRigidbody2D.velocity * killable.GetMass(), ForceMode2D.Impulse);
+
+            //Kill
+            killable.Perish();
         }
     }
 
